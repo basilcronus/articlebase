fdaf7cc41c8201a037238cec8e1f961ba31ce57c
Mike Nadasy <Mike.Nadasy.ctr@uscp.gov>
2017-10-05 10:24:50 -0400

    add form

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
+    list: function(req, res){
+        Articles.find({}).exec((err, articles) => {
+            if(err){
+                res.send(500, {error: 'database error'})
+            }
+            res.view('list', {articles: articles});
+        })
+    },
+    add: function(req, res){
+        res.view('add');
+    },
+    create: function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.create({title: title, body: body}).exec((err) => {
+            if(err){
+                res.send(500, {error: 'database error'})
+            }
+            res.redirect('/articles/list');
+        })
+    }
 	
 };
 
