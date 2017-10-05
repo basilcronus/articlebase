85151ab743b3e9baf1ba9b3b4e42ba177546daf7
Mike Nadasy <Mike.Nadasy.ctr@uscp.gov>
2017-10-05 10:44:32 -0400

    crud functionality

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -27,7 +27,37 @@ module.exports = {
             }
             res.redirect('/articles/list');
         })
-    }
-	
+    },
+    delete: function(req, res){
+        Articles.destroy({id: req.params.id}).exec((err) => {
+            if(err){
+                res.send(500, {error: 'database error'})
+            }
+            res.redirect('/articles/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Articles.findOne({id:req.params.id}).exec((err, article) => {
+            if(err){
+                res.send(500, {error: 'database error'})
+            }
+
+            res.view('edit', {article:article});
+        })
+    },
+    update: function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.update({id: req.params.id},{title: title, body: body}).exec((err) => {
+            if(err){
+                res.send(500, {error: 'database error'})
+            }
+            res.redirect('/articles/list');
+        });
+        return false;
+    }	
 };
 
